ae76675d33944cb514c54caeee09bfbec777c510
James Tranovich <jtranovich@gmail.com>
2015-10-14 15:49:06 -0700

    Drawing of titles.

--- src/Main.elm
+++ src/Main.elm
@@ -1,16 +1,22 @@
 module Main where
 
-import Graphics.Element exposing (show)
+import Text exposing (..)
 import Treemap exposing (Data, Coordinate, squarify)
-import Graphics.Collage exposing (Form, rect, move, filled, collage)
-import Color exposing (rgba)
+import Graphics.Element exposing (show)
+import Graphics.Collage exposing (Form, rect, move, text, defaultLine, filled, collage, group)
+import Color exposing (rgba, white)
 
 -- testing purposes only.
 
 type alias SampleData = List (String, Float)
 
-extract : SampleData -> Data
-extract datum = List.map snd datum |> List.sort |> List.reverse
+style = { defaultStyle | color <- white }
+
+extract : SampleData -> (List String, Data)
+extract datum =
+  List.sortBy snd datum
+    |> List.reverse
+    |> List.unzip
 
 normalize : Float -> Data -> Data
 normalize area values =
@@ -18,8 +24,8 @@ normalize area values =
       multiplier = area / total in
     List.map (\v -> v * multiplier) values
 
-draw : Coordinate -> Form
-draw c =
+draw : Coordinate -> String -> Form
+draw c title =
   let w = c.x2 - c.x1
       h = c.y2 - c.y1
       -- translate from top-left coordinates to center of element
@@ -27,20 +33,20 @@ draw c =
       y = (height / 2) - (h / 2) - c.y1
       -- temporary colors, for now
       color = rgba 255 (c.y2 |> round) 255 255
+      t = text <| Text.color white (Text.height (h / 4) (Text.fromString title))
       g = rect w h in
-  move (x, y) <| filled color g
+  group [move (x, y) <| filled color g, move(x, y) t]
 
 main =
   let area = width * height
-      data = extract testData |> normalize area
+      (titles, values) = extract testData
+      data = normalize area values
       result = squarify data { x = 0, y = 0, width = width, height = height }
-      minimum = 1
-      maximum = 6
-      rectangles = List.map draw result in
+      rectangles = List.map2 draw result titles in
   collage width height rectangles
 
-width = 200
-height = 100
+width = 400
+height = 200
 
 testData : SampleData
 testData = [ ("Area 1", 6)
